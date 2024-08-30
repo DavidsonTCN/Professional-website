6d34b16b58fb526b9323ab3f638d4b05dab11b79
DavidsonTCN <david.diei@altimex.ca>
2024-08-30 19:07:54 -0400

    Update script.js

--- test 3/script.js
+++ test 3/script.js
@@ -1,14 +1,14 @@
-var section = document.getElementById('abt');
-var display = 0;
+// var section = document.getElementById('abt');
+// var display = 0;
 
-function hideShow()
-{
-    if(display==1){
-        section.style.display = 'block';
-        display = 0;
-    }
-    else{
-        section.style.display = 'none';
-        display = 1;
-    }
-}
+// function hideShow()
+// {
+//     if(display==1){
+//         section.style.display = 'block';
+//         display = 0;
+//     }
+//     else{
+//         section.style.display = 'none';
+//         display = 1;
+//     }
+// }
